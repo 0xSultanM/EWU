2aa14ed599c54800d0987e8040cfb338d5d03e76
0xSultanM <sultanmprince@gmail.com>
2025-05-14 12:14:34 +0600

    Updated File

--- src/Design/Main_GUI.java
+++ src/Design/Main_GUI.java
@@ -55,7 +55,7 @@ public class Main_GUI extends JFrame {
         ActivityCheck.setBounds(12,50,150,50);
         WorkoutLog.setBounds(12,100,150,50);
         NutritionLog.setBounds(12,220,150,50);
-        GoalLog.setBounds(12,450,150,50);
+        GoalLog.setBounds(12,270,150,50);
 
         ActivityCheck.setBackground(new Color(0, 78, 126));
         WorkoutLog.setBackground(new Color(0, 78, 126));
@@ -122,32 +122,39 @@ public class Main_GUI extends JFrame {
 
         JLabel act = new JLabel("Select Activity Type: ");
         JLabel actTime = new JLabel("Duration (in minutes): ");
+        JLabel actDestination = new JLabel("Destination (in KMs): ");
         String[] options = { "Running", "Walking", "Cycling", "Swimming" };
         JTextField duration = new JTextField(10); 
+        JTextField destination = new JTextField(10); 
         JComboBox<String> comboBox = new JComboBox<>(options);
 
 
         act.setBounds(155, 100, 150, 30);
         actTime.setBounds(155, 150, 150, 30);
+        actDestination.setBounds(155,200,150, 30);
         comboBox.setBounds(400, 100, 150, 30);
         duration.setBounds(400, 150, 150, 30);
+        destination.setBounds(400, 200, 150, 30);
 
         act.setForeground(new Color(255, 215, 0));
         actTime.setForeground(new Color(255, 215, 0));
+        actDestination.setForeground(new Color(255, 215, 0));
 
         activity.add(act);
         activity.add(comboBox);
         activity.add(actTime);
         activity.add(duration);
+        activity.add(actDestination);
+        activity.add(destination);
 
         JButton button = new JButton("Calculate");
-        button.setBounds(300, 200, 120, 40); 
+        button.setBounds(300, 250, 120, 40); 
         activity.add(button);
 
         JTextArea resultArea = new JTextArea(10, 20);
         resultArea.setEditable(false);
         resultArea.setBackground(new Color(6, 7, 20));
-        resultArea.setForeground(new Color(255, 215, 0));
+        resultArea.setForeground(new Color(255, 25, 0));
         resultArea.setMargin(new Insets(20, 20, 20, 20));
         resultArea.setFont(new Font("Arial", Font.PLAIN, 14));
         resultArea.setText("Calories burned will be displayed here.");
@@ -156,7 +163,7 @@ public class Main_GUI extends JFrame {
         JScrollPane scrollPane = new JScrollPane(resultArea);
         scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
-        scrollPane.setBounds(155, 250, 400, 100); 
+        scrollPane.setBounds(155, 300, 400, 100); 
         activity.add(scrollPane); 
 
 
@@ -166,18 +173,19 @@ public class Main_GUI extends JFrame {
                 String selectedOption = (String) comboBox.getSelectedItem();
                 double choice;
                 if (selectedOption.equals("Running")) {
-                    choice = 2.5;
+                    choice = 125;
                 } else if (selectedOption.equals("Walking")) {
-                    choice = 3.0;
+                    choice = 213;
                 } else if (selectedOption.equals("Cycling")) {
-                    choice = 4.0;
+                    choice = 335;
                 } else {
-                    choice = 3.5;
+                    choice = 401;
                 }
 
                 try {
                     double selected = Double.parseDouble(duration.getText());
-                    double burned = choice * selected;
+                    double selected_Des = Double.parseDouble(destination.getText());
+                    double burned = choice * selected * selected_Des;
                     resultArea.setText("Calories burned: " + burned+". Data saved successfully!!");
                     saveToFile(selectedOption, selected, burned);
                 } catch (NumberFormatException ex) {
@@ -194,7 +202,7 @@ public class Main_GUI extends JFrame {
 
             
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
-                writer.write("Activity: " + activityType + ", Duration: " + duration + " minutes, Calories burned: " + burnedCalories);
+                writer.write("Activity: " + activityType + ", Duration: " + duration + "minutes, " + ", Calories burned: " + burnedCalories);
                 writer.newLine();
             } catch (IOException e) {
                 System.err.println("Error writing to file: " + e.getMessage());
@@ -202,22 +210,97 @@ public class Main_GUI extends JFrame {
         }
 
 
-
-
     private JPanel WorkoutPanel() {
         JPanel workout = new JPanel();
         workout.setBackground(new Color(6, 7, 20));
-        workout.setLayout(new FlowLayout(FlowLayout.CENTER));
+        workout.setLayout(null);
 
         JLabel Title = new JLabel("Atlas Fitness - Workout ");
         Font titleFont = new Font("SansSerif", Font.BOLD, 32);
         Title.setFont(titleFont);
         Title.setForeground(new Color(255, 215, 0));
+        Title.setBounds(155, 20, 400, 50); 
+        workout.add(Title);  
 
-        workout.add(Title);
+
+        JLabel workoutType = new JLabel("Select Workout Type: ");
+        JLabel repititions = new JLabel("Repititions: ");
+        JLabel workoutSets = new JLabel("Number of Sets: ");
+        String[] options = { "DeadLift", "Squats", "Jumping Rope", "Push-UPs" };
+        JTextField repi = new JTextField(10); 
+        JTextField ws = new JTextField(10); 
+        JComboBox<String> comboBox = new JComboBox<>(options);
+
+
+        workoutType.setBounds(155, 100, 150, 30);
+        repititions.setBounds(155, 150, 150, 30);
+        workoutSets.setBounds(155,200,150, 30);
+        comboBox.setBounds(400, 100, 150, 30);
+        repi.setBounds(400, 150, 150, 30);
+        ws.setBounds(400, 200, 150, 30);
+
+        workoutType.setForeground(new Color(255, 215, 0));
+        repititions.setForeground(new Color(255, 215, 0));
+        workoutSets.setForeground(new Color(255, 215, 0));
+
+        workout.add(workoutType);
+        workout.add(comboBox);
+        workout.add(repititions);
+        workout.add(repi);
+        workout.add(workoutSets);
+        workout.add(ws);
+
+        JButton button = new JButton("Save Info");
+        button.setBounds(300, 250, 120, 40); 
+        workout.add(button);
+
+        JTextArea resultArea = new JTextArea(10, 20);
+        resultArea.setEditable(false);
+        resultArea.setBackground(new Color(6, 7, 20));
+        resultArea.setForeground(new Color(255, 25, 0));
+        resultArea.setMargin(new Insets(20, 20, 20, 20));
+        resultArea.setFont(new Font("Arial", Font.PLAIN, 14));
+        resultArea.setText("");
+
+       
+        JScrollPane scrollPane = new JScrollPane(resultArea);
+        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+        scrollPane.setBounds(155, 300, 400, 100); 
+        workout.add(scrollPane); 
+
+
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String selectedOption = (String) comboBox.getSelectedItem();
+                double selected_repi = Double.parseDouble(repi.getText());
+                double selected_sets = Double.parseDouble(ws.getText());
+
+                try {
+                    resultArea.setText("Data saved successfully!!");
+                    saveToFileWO(selectedOption, selected_repi, selected_sets);
+                } catch (NumberFormatException ex) {
+                    resultArea.setText("Invalid input. Please enter a valid number for duration.");
+                }
+            }
+        });
+
 
         return workout;
     }
+
+    private void saveToFileWO(String Exercise, double Repitition, double CompleteSet) {
+            File file = new File("data.txt");
+
+            
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+                writer.write("Exercise Name: " + Exercise + ", Repitition: " + Repitition + "times, and number of sets: " + CompleteSet);
+                writer.newLine();
+            } catch (IOException e) {
+                System.err.println("Error writing to file: " + e.getMessage());
+            }
+        }
 
     private JPanel NutritionPanel() {
         JPanel nutrition = new JPanel();
@@ -232,8 +315,8 @@ public class Main_GUI extends JFrame {
         nutrition.add(Title);  
         
 
-        JLabel nutri_amount = new JLabel("Ammount of Food (CUP): ");
-        JLabel nutri_type = new JLabel("Select the Food: ");
+        JLabel nutri_amount = new JLabel("Select the Food: ");
+        JLabel nutri_type = new JLabel("Ammount of Food: ");
         JTextField amount = new JTextField(10);
         String[] options = { "Rice", "Milk", "Meat", "Vegetables" };
         JComboBox<String> comboBox = new JComboBox<>(options);
